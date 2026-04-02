fd94121caee36608248229dee7992f22b7ee3652
netgoatfr <netgoatfr@gmail.com>
2026-04-02 22:43:08 +0200

    push

--- projects/jar_structure_fixer/index.js
+++ projects/jar_structure_fixer/index.js
@@ -1,4 +1,6 @@
 
+import { unzip } from 'https://cdn.skypack.dev/fflate@0.8.2?min';
+
 // --------- META LOADING ----------
 async function loadMeta() {
   try {
@@ -16,7 +18,11 @@ async function loadMeta() {
 // --------- HELPERS -------------
 const state = {
 };
-
+/** 
+ * @param {string} id
+ * @param {string} text
+ * @returns {string}
+ */
 function button(id, text) {
   return `<button id="${id}">${text}</button>`;
 }
@@ -25,22 +31,60 @@ function button(id, text) {
 function setApp(html) {
   document.getElementById("app").innerHTML = html;
 }
-
 function setControls(html) {
   document.getElementById("controls").innerHTML = html;
 }
-
 function setInfo(html) {
   document.getElementById("info").innerHTML = html;
 }
 
 
+
 // --------- PROJECT CODE ----------
+function checkApi()
+{ 
+    //Check support for the File API support 
+    if ( window.File && window.FileReader && window.FileList && window.Blob )
+    {
+        var fileSelected = document.getElementById( "inputFile" );
+        fileSelected.addEventListener( "change", handleFile, false );
+    } 
+    else
+    { 
+        alert( "Files are not supported" ); 
+    } 
+}
+
+async function handleFile(ev) {
+  const file = /** @type {HTMLInputElement } */ (ev.target).files.item(0)
+  if (!file) {
+    alert("Invalid file")
+    return
+  }
+  
+  var buffer = await file.bytes()
+  var fileReader = new FileReader(); 
+  fileReader.onload = function ( e )
+  { 
+
+    unzip(e.target, (err, files) => {
+      if (err) throw err;
+
+      const content = new TextDecoder().decode(files['script.js']);
+      console.log(content);
+    });
+  } 
+  fileReader.readAsArrayBuffer( buffer );
+
+}
+
 function init() {
 
   setApp(`
     <h1>Jar Structure Fixer</h1>
-    <div class="counter">
+    <div class="fixer">
+      <div id="openFile"><input type="file" id="inputFile" /></div>
+
       <button id="dec">-</button>
       <span id="value">0</span>
       <button id="inc">+</button>
